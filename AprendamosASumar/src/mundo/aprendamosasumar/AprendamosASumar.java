d6afc6bfce6d0e7f5992edfa9048422640bd457b
jiselaandrea <jiselaandrea@gmail.com>
2015-11-21 15:58:09 -0500

    prueba

---
+++ AprendamosASumar/src/mundo/aprendamosasumar/AprendamosASumar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mundo.aprendamosasumar;
+
+/**
+ *
+ * @author Andrea
+ */
+public class AprendamosASumar {
+    
+    Usuario usuario;
+    
+    public void determinarNivel(){
+        
+    }
+    
+    public void cambiarNivel(){
+        
+    }
+    
+}
