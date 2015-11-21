5e1d845a2f44edc9be9241591720d7021f4f28b5
jiselaandrea <jiselaandrea@gmail.com>
2015-11-21 10:11:05 -0500

    prueba

--- AprendamosASumar/src/mundo/aprendamosasumar/ConexionDB.java
+++ AprendamosASumar/src/mundo/aprendamosasumar/ConexionDB.java
@@ -16,13 +16,13 @@ public class ConexionDB {
     public static void main (String args []) throws SQLException{    
         DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
         Connection conn = DriverManager.getConnection
-              ("jdbc:oracle:thin:@localhost:1521:XE", "ia", "123");
-             // driver@machineName:port:SID           ,  userid,  password
-        Statement stmt = conn.createStatement();
-        ResultSet rset = 
-                  stmt.executeQuery("select BANNER from SYS.V_$VERSION");
-        while (rset.next())
-             System.out.println (rset.getString(1));   // Print col 1
-        stmt.close();  
-    }
+        ("jdbc:oracle:thin:@localhost:1521:XE", "ia", "123");
+        try (
+            Statement stmt = conn.createStatement()){
+            ResultSet rset =
+            stmt.executeQuery("select BANNER from SYS.V_$VERSION");
+            while (rset.next())
+            System.out.println (rset.getString(1));   // Print col 1
+            }          
+    }    
 }
