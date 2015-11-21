bd7c6a70c2d5020b9d87e88c54d20f2cca49352d
jiselaandrea <jiselaandrea@gmail.com>
2015-11-21 14:21:47 -0500

    prueba

--- AprendamosASumar/src/mundo/aprendamosasumar/Login.java
+++ AprendamosASumar/src/mundo/aprendamosasumar/Login.java
@@ -18,25 +18,36 @@ public class Login {
 public static ConexionDB baseDatos=new ConexionDB();
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String usuario,clave;
         usuario=JOptionPane.showInputDialog("Ingrese Usuario");
         clave=JOptionPane.showInputDialog("Ingrese contraseña");
         //Usuario objUsuario=new Usuario();
         logeo(usuario,clave);
-    }
-//     
+    }     
 
-    private static void logeo(String usuario, String clave) {
+    private static void logeo(String usuario, String clave) throws SQLException {
+        ResultSet resultados = null;
         baseDatos.conectar();
-        ResultSet resultados=baseDatos.consultar("Select  From USUARIOS WHERE NOMBREUSUARIO="+usuario+"and password="+clave);
+
+        resultados=baseDatos.consultar("Select * From USUARIOS WHERE NOMBREUSUARIO= "+usuario+"and password="+clave);
+        while (resultados.next()) {
+            System.out.println(""+resultados.getBigDecimal("CODUSUARIO")+"       "+resultados.getString("NOMBREUSUARIO"));
+        }
+        if (resultados != null){
+            JOptionPane.showMessageDialog(null, "Bienvenido "+ usuario);
+
+        resultados=baseDatos.consultar("Select  From USUARIOS WHERE NOMBREUSUARIO="+usuario+"and password="+clave);
         if (resultados!= null){
             JOptionPane.showMessageDialog(null, "Bienvenido"+usuario);
+
         }       
-        else{ JOptionPane.showMessageDialog(null, "Usuario no existe o combinacion erronea");
+        else{ 
+            JOptionPane.showMessageDialog(null, "Usuario no existe o combinacion erronea");
+        }
     }
-    }
+ }
    
     
-        }
+ 
 
