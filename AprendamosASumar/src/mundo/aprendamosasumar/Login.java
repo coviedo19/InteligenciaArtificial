22aba023fcef79efba97d943fc5766dddc5b5ab7
jiselaandrea <jiselaandrea@gmail.com>
2015-11-21 11:30:19 -0500

    prueba

--- AprendamosASumar/src/mundo/aprendamosasumar/Login.java
+++ AprendamosASumar/src/mundo/aprendamosasumar/Login.java
@@ -15,39 +15,6 @@ import java.sql.*;
  */
 public class Login {
        
-    /*
-    * Ejecuta sentencias en la base de datos
-    */
-    public boolean ejecutar(String sql) {
-        try{
-            Statement sentencia;
-            sentencia = getConexion().createStatement(ResultSet.TYPE_FORWARD_ONLY,  ResultSet.CONCUR_READ_ONLY);
-            sentencia.executeUpdate(sql);
-            getConexion().commit();
-            sentencia.close();
-        }catch(SQLException e){
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
     
-    
-    /*
-    * Realiza consultas en la base de datos
-    */
-    public ResultSet consultar(String sql) {
-        ResultSet resultado = null;
-        try {
-            Statement sentencia;
-            sentencia = getConexion().createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            resultado = sentencia.executeQuery(sql);
-            getConexion().commit();
-        } catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }        
-        return resultado;
-    }
         
 }
