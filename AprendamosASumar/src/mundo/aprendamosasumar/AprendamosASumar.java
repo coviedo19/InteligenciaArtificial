3e5bdec7c576df342c87af790b02f40066ef0f09
jiselaandrea <jiselaandrea@gmail.com>
2015-11-27 00:58:17 -0500

    Capturar dato - OK

--- AprendamosASumar/src/mundo/aprendamosasumar/AprendamosASumar.java
+++ AprendamosASumar/src/mundo/aprendamosasumar/AprendamosASumar.java
@@ -5,20 +5,38 @@
  */
 package mundo.aprendamosasumar;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Andrea
  */
 public class AprendamosASumar {
     
-    Usuario usuario;
+    //Usuario usuario;
+    //int codigoUsuario = usuario.getCODUSUARIO();
+    //int nivelUsuario = usuario.getCODNIVEL();
     
-    public void determinarNivel(){
-        
-    }
+    int resultadoImagen;
+    
+    
+//    public void determinarNivel(){
+//        
+//        if(nivelUsuario == 0){
+//            
+//        }
+//    }
     
     public void cambiarNivel(){
         
+    }
+    
+    public void validarRespuesta(int valor){
+        if(valor == 3){
+            JOptionPane.showMessageDialog(null, "Es tres");
+        }else{
+           JOptionPane.showMessageDialog(null, "No es tres"); 
+        }           
     }
     
 }
